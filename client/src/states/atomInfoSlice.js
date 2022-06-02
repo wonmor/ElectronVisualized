d4f85a7b523e1632bb2682d11bb8b730d6998637
John Seong <wonmor@gmail.com>
2022-06-02 13:10:59 -0400

    Updated functions' comments

--- client/src/states/atomInfoSlice.js
+++ client/src/states/atomInfoSlice.js
@@ -1,6 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 export const atomInfoSlice = createSlice({
+    /*
+    This function creates a new reducer (takes an action and the previous state of the application and returns the new state)
+    for React-Redux library (acts as a global variable)
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    */
     name: 'atomInfo',
     initialState: {
         value: null
--- client/src/states/counterSlice.js
+++ client/src/states/counterSlice.js
@@ -1,6 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 export const counterSlice = createSlice({
+    /*
+    This function creates a new reducer (takes an action and the previous state of the application and returns the new state)
+    for React-Redux library (acts as a global variable)
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    */
     name: 'counter',
     initialState: {
         value: 0,
