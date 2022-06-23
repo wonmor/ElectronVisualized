6a870ab0f72cdeef53395748ccea4a785b076e2a
John Seong <wonmor@gmail.com>
2022-06-22 20:17:49 -0400

    Added files for Electron-based Desktop App
    - Added support for macOS using Electron

---
+++ desktop/renderer.js
@@ -0,0 +1,12 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+const { ipcRenderer } = require("electron");
+
+document.querySelector('#reload').addEventListener('click', function() {
+    ipcRenderer.send("reload");
+})
