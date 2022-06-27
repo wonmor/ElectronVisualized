65e7acb56bd5f65230d71ced90bbb42f541692b6
John Seong <wonmor@gmail.com>
2022-06-27 13:21:36 -0400

    Update electron_visualized.py

--- electron_visualized.py
+++ electron_visualized.py
@@ -5,16 +5,13 @@
 ▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▀▄▄▄▀▀▄▄▀▀▀▀▀▄▀▀▀▄▄▄▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▀
 ----------------------------------------------------------------------------
 Purpose:     An interactive module that visualizes the electron configurations of different elements.
-             Displayed in an orbital format, based upon the internal calculations performed that involve the famous Dirac equation,
-             which describes the shape and structure of the electrons in terms of the wavefunction.
+             Displayed in an orbital format, based upon the internal calculations performed that involve the famous Density Functional Theory,
+             which simplifies the N-dimensional Schrödinger's Euqation problem into a three-dimensional one.
 Author:      John Seong
 Created:     23-Mar-2022
-Updated:     31-May-2022
 -----------------------------------------------------------------------------
 I think this project deserves a level 4+ because...
-1. A real-time electron movement simulation that no other program offers in a non-scientific field of software engineering 
-      - derived by the electrons' angular momentum formula
-2. Each dot represent a probable location that the electron might reside at based upon the wavefunction 
+1. Each dot represent a probable location that the electron might reside at based upon electron density at a given location 
       — according to the Density Functional Theory (DFT)
 3. Used industry standard Quantum Mechanics simulation libaries such as ASE and GPAW for numerically computing the electron density
 -----------------------------------------------------------------------------
