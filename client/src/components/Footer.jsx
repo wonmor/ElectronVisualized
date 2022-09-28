5af2373a9b882303aadab51776c8ae6e97d49fce
John Seong <wonmor@gmail.com>
2022-09-28 12:02:59 -0400

    Added David Chi as a collaborator

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,10 +14,10 @@ export default function Footer() {
         A HTML markup that contains graphical elements
     */
     return (
-        <div className="p-3 text-white text-center bg-gray-600">
+        <div className="p-3 text-gray-400 text-center bg-gray-600">
             <div className="scale-90 sm:scale-100">
                 <span className="text-xl">
-                    © 2022 Licensed under MIT. Developed by <a className="hover:underline text-blue-200" href="https://github.com/wonmor" target="_blank" rel="noopener noreferrer">John Seong</a>.
+                    © 2022 Licensed under <a className="hover:underline text-white" href="https://en.wikipedia.org/wiki/MIT_License" target="_blank" rel="noopener noreferrer">MIT</a>. Developed by <a className="hover:underline text-white" href="https://github.com/wonmor" target="_blank" rel="noopener noreferrer">John Seong</a> and <a className="hover:underline text-white" href="https://github.com/davidchi31415" target="_blank" rel="noopener noreferrer">David Chi</a>.
                 </span>
             </div>
         </div>
