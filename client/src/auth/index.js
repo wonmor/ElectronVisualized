12f23aa4678036382617677d21abdd50bb783e52
John Seong <wonmor@gmail.com>
2023-03-17 12:08:22 -0400

    Update index.js

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,41 +1,12 @@
-import { createAuthProvider } from 'react-token-auth';
+import {createAuthProvider} from 'react-token-auth';
 
-const authProvider = {
-    [Symbol.iterator]: function* () {
-      yield this.useAuth;
-      yield this.authFetch;
-      yield this.login;
-      yield this.logout;
-    },
-    useAuth: null,
-    authFetch: null,
-    login: null,
-    logout: null,
-  };
-  
-  const provider = createAuthProvider({
-    accessTokenKey: 'access_token',
-    onUpdateToken: async (token) => {
-      const response = await fetch(
-        process.env.NODE_ENV === 'production'
-          ? '/api/refresh'
-          : 'https://www.electronvisual.org/api/refresh',
-        {
-          method: 'POST',
-          body: token.access_token,
-        }
-      );
-      const data = await response.json();
-      return data;
-    },
-  });
-  
-  provider.then(([useAuth, authFetch, login, logout]) => {
-    authProvider.useAuth = useAuth;
-    authProvider.authFetch = authFetch;
-    authProvider.login = login;
-    authProvider.logout = logout;
-  });
-  
-  export const { useAuth, authFetch, login, logout } = authProvider;
-  
+
+export const { useAuth, authFetch, login, logout } =
+    createAuthProvider({
+        accessTokenKey: 'access_token',
+        onUpdateToken: (token) => fetch('/api/refresh', {
+            method: 'POST',
+            body: token.access_token
+        })
+        .then(r => r.json())
+    });
