c8d9973ff752b4be6b50a66ce0603b50993896c7
John Seong <wonmor@gmail.com>
2023-03-17 11:04:09 -0400

    Update index.js

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,19 +1,48 @@
 import { createAuthProvider } from 'react-token-auth';
 
-const authProvider = createAuthProvider({
-  accessTokenKey: 'access_token',
-  onUpdateToken: (token) =>
-    fetch(
-      process.env.NODE_ENV === 'production'
-        ? '/api/refresh'
-        : 'https://www.electronvisual.org/api/refresh',
-      {
-        method: 'POST',
-        body: token.access_token,
-      }
-    ).then((r) => r.json()),
-});
-
-console.log(authProvider);
-
-export const [useAuth, authFetch, login, logout] = authProvider;
+const authProvider = {
+    [Symbol.iterator]: function* () {
+      yield this.useAuth;
+      yield this.authFetch;
+      yield this.login;
+      yield this.logout;
+    },
+    useAuth: null,
+    authFetch: null,
+    login: null,
+    logout: null,
+  };
+  
+  createAuthProvider({
+    accessTokenKey: 'access_token',
+    onUpdateToken: (token) =>
+      fetch(
+        process.env.NODE_ENV === 'production'
+          ? '/api/refresh'
+          : 'https://www.electronvisual.org/api/refresh',
+        {
+          method: 'POST',
+          body: token.access_token,
+        }
+      ).then((r) => r.json()),
+  }).forEach((fn, i) => {
+    switch (i) {
+      case 0:
+        authProvider.useAuth = fn;
+        break;
+      case 1:
+        authProvider.authFetch = fn;
+        break;
+      case 2:
+        authProvider.login = fn;
+        break;
+      case 3:
+        authProvider.logout = fn;
+        break;
+      default:
+        break;
+    }
+  });
+  
+  export const [useAuth, authFetch, login, logout] = authProvider;
+  
