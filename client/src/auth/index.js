003216a1c9aaaecb8f5cb4c88dde2b2c4cd6c1ed
John Seong <wonmor@gmail.com>
2023-03-17 09:48:27 -0400

    Update index.js

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,12 +1,19 @@
 import { createAuthProvider } from 'react-token-auth';
 
+const authProvider = createAuthProvider({
+  accessTokenKey: 'access_token',
+  onUpdateToken: (token) =>
+    fetch(
+      process.env.NODE_ENV === 'production'
+        ? '/api/refresh'
+        : 'https://www.electronvisual.org/api/refresh',
+      {
+        method: 'POST',
+        body: token.access_token,
+      }
+    ).then((r) => r.json()),
+});
 
-export const [useAuth, authFetch, login, logout] =
-    createAuthProvider({
-        accessTokenKey: 'access_token',
-        onUpdateToken: (token) => fetch('/api/refresh', {
-            method: 'POST',
-            body: token.access_token
-        })
-        .then(r => r.json())
-    });
+console.log(authProvider);
+
+export const [useAuth, authFetch, login, logout] = authProvider;
