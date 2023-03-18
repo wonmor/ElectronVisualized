125a75cec506416f5a55e01b5d9199ea45e5cc9e
John Seong <wonmor@gmail.com>
2023-03-18 11:46:46 -0400

    Update Table.jsx

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -383,7 +383,7 @@ export default function Table() {
               <div className="flex flex-col md:flex-row justify-center items-center">
                 <button
                   onClick={() => {movePage("/login")}}
-                  className="w-fit text-white bg-[#050708]/30 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
+                  className="w-fit text-white bg-[#050708]/30 hover:bg-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
                 >
                   <span>
                     Get Pro Membership
