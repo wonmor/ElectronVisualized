7f22b661129922cc88c0026acbdb73c01fcadbfc
John Seong <wonmor@gmail.com>
2023-04-24 20:16:02 -0400

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,87 +1,96 @@
-import { Background } from "./Geometries";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
-import MetaTag from "./MetaTag";
-import firebase from 'firebase/compat/app';
-import * as firebaseui from 'firebaseui';
+export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-import 'firebase/compat/auth';
-import 'firebaseui/dist/firebaseui.css';
+  const navigate = useNavigate();
 
-export default function Register() {
-    const [errorMessage, setErrorMessage] = useState("");
-    const authContainer = useRef(null);
+  const isValidEmail = (email) => {
+    // You can use any email validation regex you prefer
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return regex.test(email);
+  };
 
-    const navigate = useNavigate();
+  const isValidPassword = (password) => {
+    // This is a basic password validation, you can update it to include more rules
+    return password.length >= 8;
+  };
 
-    const uiConfig = {
-        signInFlow: 'popup',
-        signInSuccessUrl: '/membership',
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ],
-        callbacks: {
-          signInSuccessWithAuthResult: () => {
-            navigate('/membership');
-            return false; // Prevents redirect by FirebaseUI
-          },
-        },
-    };
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!isValidEmail(email.trim())) {
+      setError("Invalid email address");
+      return;
+    }
+    if (!isValidPassword(password)) {
+      setError("Password must be at least 8 characters long");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+    try {
+      await firebase.auth().createUserWithEmailAndPassword(email.trim(), password);
+      navigate("/membership");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
-    useEffect(() => {
-        const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
-        ui.start(authContainer.current, uiConfig);
+  const handleGoogleSignUp = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      await firebase.auth().signInWithPopup(provider);
+      navigate("/membership");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
-        const unregisterAuthObserver = firebase.auth().onAuthStateChanged((user) => {
-          if (user) {
-            navigate('/membership');
-          }
-        });
-
-        return () => {
-          unregisterAuthObserver();
-          ui.reset();
-        };
-    }, [navigate]);
-
-    return (
-        <>
-          <MetaTag title={"ElectronVisualized"}
-            description={"View Electron Density, Molecular and Atomic Orbitals"}
-            keywords={"electron, electron density, chemistry, computational chemistry"}
-            imgsrc={"cover.png"}
-            url={"https://electronvisual.org"} />
-    
-          <div className="bg-gray-700 pb-5 overflow-auto" style={{ minHeight: "100vh", width: "-webkit-fill-available" }}>
-            <div className="text-center pt-10 pl-5 pr-5 text-gray-400">
-              <h1 className="sm:pb-5 scale-75 sm:scale-100">
-                <span className="text-white">{"Sign up"}</span>
-              </h1>
-
-              <h3>or <button onClick={() => {
-                navigate("/login");
-              }}><span className="text-blue-200 hover:underline">{"Sign in"}</span></button>.</h3>
-
-              <div ref={authContainer}></div>
-
-              {errorMessage !== "" && (
-                <div className="mt-5">
-                    <h3 className="text-red-200">
-                        {errorMessage}
-                    </h3>
-                </div>
-              )}
-
-              <div className="max-w-lg m-auto pt-5">
-                  <span className="text-center text-gray-400">
-                      Sign up for our membership today and start exploring the exciting world of atomic and molecular orbitals like never before!
-                  </span>
-              </div>
-            </div>
-          </div>
-          <Background />
-        </>
-    );
+  return (
+    <div className="bg-gray-700 pb-5 overflow-auto" style={{ minHeight: "100vh", width: "-webkit-fill-available" }}>
+      <div className="text-center pt-10 pl-5 pr-5 text-gray-400">
+        <h1 className="sm:pb-5 scale-75 sm:scale-100">
+          <span className="text-white">{"Sign Up"}</span>
+        </h1>
+        <form onSubmit={handleSignUp}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="block mt-4 lg:inline-block lg:mt-0 mr-4 text-white"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="block mt-4 lg:inline-block lg:mt-0 mr-4 text-white"
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="block mt-4 lg:inline-block lg:mt-0 mr-4 text-white"
+          />
+          <button type="submit" className="block mt-4 lg:inline-block lg:mt-0 mr-4 text-white">
+            Sign Up
+          </button>
+        </form>
+        <button onClick={handleGoogleSignUp} className="block mt-4 lg:inline-block lg:mt-0 mr-4 text-white">
+          Sign Up with Google
+        </button>
+        {error && <p className="text-red-200">{error}</p>}
+        </div>
+    </div>
+  );
 }
