2745f70644f9127d3152cc2ce747d132d6138ed1
John Seong <wonmor@gmail.com>
2023-04-27 00:34:01 -0400

    Update Instances.jsx

--- client/src/components/Instances.jsx
+++ client/src/components/Instances.jsx
@@ -73,9 +73,9 @@ export function Particles({ particleRef, lightRef, particleRadius }) {
         );
 
         // Phase shift and scale the coordinates to match the existing molecule shape that is already generated....
-        const x = coords[0] / 5 - 22.5
-        const y = coords[1] / 5 - 22.5
-        const z = coords[2] / 5 - 22.5
+        const x = coords[0] / 5 - 22.0
+        const y = coords[1] / 5 - 22.0
+        const z = coords[2] / 5 - 22.0
 
         temp.push({ x, y, z, volume, isColourException });
       }
